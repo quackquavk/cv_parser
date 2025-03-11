9b3c4402b4789fa5318175a194ca14b4aa557349
quackquavk <pukarkhanal4@gmail.com>
2025-03-11 16:12:47 +0545

    populate by name

--- models/document.py
+++ models/document.py
@@ -13,7 +13,7 @@ class Document(BaseModel):
     class Config:
         populate_by_name = True
         json_encoders = {ObjectId: str}
-        allow_population_by_field_name = True
+        populate_by_name = True
 
 
 class DocumentResponse(BaseModel):
